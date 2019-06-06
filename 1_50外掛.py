685a535565aca066a08c78fdd24274ce674a54ab
funny4875 <funny4875@gmail.com>
2019-06-07 03:13:27 +0800

    Add files via upload

---
+++ 1_50外掛.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+#game site:
+#http://wap.jue-huo.com/app/html/game/1to50/1to50.html
+
+import pyautogui as ag
+for i in range(1,51):
+    A=ag.locateCenterOnScreen(str(i)+'.png',confidence=0.92)
+    ag.moveTo(A,duration=0.002)
+    ag.click(A)
+
