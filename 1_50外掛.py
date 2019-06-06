5bb53d46be24a1cafbc026a3e2ba062d9c5ed994
funny4875 <funny4875@gmail.com>
2019-06-07 03:19:39 +0800

    Update 1_50外掛.py

--- 1_50外掛.py
+++ 1_50外掛.py
@@ -8,8 +8,11 @@
 #http://wap.jue-huo.com/app/html/game/1to50/1to50.html
 
 import pyautogui as ag
-for i in range(1,51):
-    A=ag.locateCenterOnScreen(str(i)+'.png',confidence=0.92)
-    ag.moveTo(A,duration=0.002)
-    ag.click(A)
-
+def autoGame():
+    try:
+        for i in range(1,51):
+            A=ag.locateCenterOnScreen(str(i)+'.png',confidence=0.92)
+            ag.moveTo(A,duration=0.002)
+            ag.click(A)
+    except: return False
+    return True
